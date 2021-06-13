be1e76bf1ad9fc23201ed8f226a1bfa913c4d978
Costigan-Stephen <stephenjcostigan@hotmail.com>
2021-06-13 00:12:20 -0500

    JSON pull

--- controllers/ta.js
+++ controllers/ta.js
@@ -1,7 +1,8 @@
 //TA05 PLACEHOLDER
 const express = require('express');
 const router = express.Router();
-const https = require('https');
+const fetch = require('node-fetch');
+
 
 const JSON_PARSE = require("../models/json");
 const MAX_PER_PAGE = 15;
@@ -24,17 +25,41 @@ exports.getTA05 = (req, res, next) => {
 };
 
 exports.getTA08 = (req, res, next) => {
-    const page = req.query.page;
-    var objects = [];
-    var request = https.get(JSON_URL, function(response) {
-        var body = "";
-        response.on("data", function(chunk) { body += chunk; });
-    });
+    var page = req.query.page;
+    var returnobject = [];
+    var object = [];
+    var totalPages = 0;
+    let settings = { method: "Get" };
+    fetch(JSON_URL, settings)
+        .then(res => res.json())
+        .then((json) => {
+            return json;
+        })
+        .then((json) => {
+            object = JSON.parse(JSON.stringify(json));
+            if (!page) {
+                page = 1;
+            }
+            var x = 0;
+            for (var i = 0; i < Object.keys(object).length; i++) {
+                if (i >= MAX_PER_PAGE * (page - 1) && i <= MAX_PER_PAGE * (page)) {
+                    returnobject[x] = object[i];
+                    x++;
+                }
+                if (i % MAX_PER_PAGE == 0) {
+                    totalPages++;
+                }
+            }
+            console.log(returnobject);
 
-    console.log(JSON.stringify(request));
+            res.render('pages/ta08', {
+                title: 'Team Activity 08',
+                path: '/ta08',
+                page: page,
+                totalPages: totalPages,
+                objects: returnobject,
+                objectsize: Object.keys(returnobject).length
+            });
+        });
 
-    res.render('pages/ta08', {
-        title: 'Team Activity 08',
-        path: '/ta08'
-    });
 }
